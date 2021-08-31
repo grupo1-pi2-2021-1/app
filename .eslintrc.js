1d2efb68940016c876ca3f8d14464d25205a4f31
Guilherme Banci <guibanci@gmail.com>
2021-08-31 00:04:30 -0300

    installing main dependencies
    Signed-off-by: Guilherme Banci <guibanci@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'react/prop-types': ['error', {ignore: ['navigation', 'route']}],
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-curly-newline': 'off',
+    'react/jsx-closing-bracket-location': 'off',
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+      'babel-module': {},
+    },
+  },
+  globals: {
+    __DEV__: true,
+  },
 };
+
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  app
+//
+//  Created by Guilherme Banci on 30/08/21.
+//
+
+import Foundation
