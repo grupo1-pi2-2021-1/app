020b231c1f183600ceba1f3e0fada277e3a4c213
Guilherme Banci <guibanci@gmail.com>
2021-10-10 20:05:33 -0300

    adding welcome image
    Signed-off-by: Guilherme Banci <guibanci@gmail.com>

--- src/pages/InitialPages/Welcome/index.js
+++ src/pages/InitialPages/Welcome/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Image} from 'react-native';
+import welcomeImage from 'assets/welcomeImage.png';
 import InitialPageTemplate from 'components/InitialPageTemplate';
 
 const Welcome = ({navigation}) => {
@@ -6,12 +8,22 @@ const Welcome = ({navigation}) => {
     navigation.navigate('UserInfos');
   };
 
+  const imageStype = {
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+    marginBottom: 30,
+  };
+
+  const WelcomeImage = () => <Image source={welcomeImage} style={imageStype} />;
+
   return (
     <InitialPageTemplate
       title="Bem vindo ao app do Esterilizador de Ambulâncias"
       subtitle="Realize procedimentos de higienização e esterilização em ambulânicas de forma simples e remota com nosso higienizador"
       buttonTitle="Começar"
       onPress={onPress}
+      Image={WelcomeImage}
       step={0}
     />
   );
